7398ab26c61946cf5bc179cd7330753a8489a34d
jens <jens.penny@gmail.com>
2020-12-20 23:58:06 +0100

    day 14. Learnt something about sparse arrays in javascript

--- src/day14.ts
+++ src/day14.ts
@@ -1,23 +1,27 @@
 import { match } from "assert";
+import { start } from "repl";
 import { FileReader } from "./tools/filereader";
 
-let test = `mask = XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X
+let testA = `mask = XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X
 mem[8] = 11
 mem[7] = 101
 mem[8] = 0`
 
-/*
-day14A(test);
+day14A(testA);
 let reader = new FileReader;
 reader.read('./res/day14')
     .then(data => day14A(data))
     .catch(err => console.error(err));
-*/
+
 let testB = `mask = 000000000000000000000000000000X1001X
 mem[42] = 100
 mask = 00000000000000000000000000000000X0XX
 mem[26] = 1`
 day14B(testB);
+reader.read('./res/day14')
+    .then(data => day14B(data))
+    .catch(err => console.error(err));
+
 
 function day14A(input: string) {
     let lines = input.split('\n').map(line => line.split('=').map(entry => entry.trim()));
@@ -62,7 +66,7 @@ function day14B(input: string) {
     let lines = input.split('\n').map(line => line.split('=').map(entry => entry.trim()));
 
     let mask = '';
-    let buffer: string[] = [];
+    let buffer: Map<number, string> = new Map();
 
     lines.forEach(line => {
         if (line[0] == 'mask') {
@@ -74,28 +78,49 @@ function day14B(input: string) {
                 return;
             } else {
                 let startIndex = parseInt(matches[0]);
-                let currentOffset = 0;
-                let allXes = mask.match(/X/g);
-                console.log(allXes);
-                let value = parseInt(line[1]).toString(2)
+                let value = startIndex.toString(2)
                     .padStart(mask.length, '0')
                     .split('')
                     .map((element, index) => {
                         if (mask[index] == 'X') {
-                            return element;
+                            return 'X';
+                        } else if (mask[index] == '1') {
+                            return '1';
                         } else {
-                            return mask[index];
+                            return element;
                         }
                     }).join('');
-                //console.log('old: ' + line[1]);
-                //console.log('new: ' + value);
-                buffer[startIndex] = value;
+
+                let perms = findPermutations(value);
+
+                //Hurr durr sparse arrays gaan `maar` tot 4294967296 - 1, wat 32 bits zijn. 
+                //Mem adresses in die opdracht zijn 36 bits
+                perms.forEach((perm) => buffer.set(parseInt(perm, 2), line[1]));
             }
         }
     });
 
-    let result = buffer.map(line => parseInt(line, 2))
-        .reduce((acc, current) => acc += current);
+    let result = 0;
+    buffer.forEach(line => result += parseInt(line, 10));
 
-    console.log('result A: ' + result)
+    console.log('result B: ' + result);
+}
+
+function findPermutations(perms: string): string[] {
+    let index = perms.lastIndexOf('X');
+
+
+    if (index == -1) {
+        return [perms];
+    } else {
+        let baseString = perms.substring(index + 1, perms.length);
+        let postparts = findPermutations(perms.substring(0, index));
+
+        let results: string[] = [];
+        postparts.forEach(part => {
+            results.push(part + '0' + baseString);
+            results.push(part + '1' + baseString);
+        });
+        return results;
+    }
 }
